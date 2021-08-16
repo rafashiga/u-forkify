cdf64d21f1fdddb578832d1c35b628b1ae613de2
Rafael <rafa.shiga@gmail.com>
2021-08-16 15:28:21 -0300

    refactor: create separate method for render ingredient and add format quantity

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,3 +1,4 @@
+import { Fraction } from 'fractional';
 import icons from 'url:../../img/icons.svg';
 
 class RecipeView {
@@ -86,21 +87,7 @@ class RecipeView {
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
           ${this.#data.ingredients
-            .map(
-              ingredient =>
-                `<li class="recipe__ingredient">
-                  <svg class="recipe__icon">
-                    <use href="${icons}#icon-check"></use>
-                  </svg>
-                  <div class="recipe__quantity">
-                  ${ingredient.quantity ?? ''}
-                  </div>
-                  <div class="recipe__description">
-                    <span class="recipe__unit">${ingredient.unit}</span>
-                    ${ingredient.description}
-                  </div>
-                </li>`
-            )
+            .map(this.#generateMarkupIngredient)
             .join('')}
         </ul>
       </div>
@@ -126,6 +113,22 @@ class RecipeView {
         </a>
       </div>
     `;
+  }
+
+  #generateMarkupIngredient(ingredient) {
+    return `
+    <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">
+      ${ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''}
+      </div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ingredient.unit}</span>
+        ${ingredient.description}
+      </div>
+    </li>`;
   }
 }
 
