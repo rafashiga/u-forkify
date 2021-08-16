f3a9b4cd5532c06f48f3721ef9bbf85f36794746
Rafael <rafa.shiga@gmail.com>
2021-08-16 16:06:02 -0300

    feat: add timeout on request

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,14 +3,6 @@ import 'regenerator-runtime/runtime';
 import * as model from './model';
 import recipeView from './views/recipeView';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 ///////////////////////////////////////
 
 const RecipesController = async () => {
