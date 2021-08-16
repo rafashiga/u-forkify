350e10f4e419e3d41423c2d6f6d7484e29724ad6
Rafael <rafa.shiga@gmail.com>
2021-08-16 14:09:44 -0300

    feat: add listen for load and hashchange event

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,11 +30,15 @@ const renderSpinner = parentEl => {
 
 const showRecipe = async () => {
   try {
-    //loading
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
+    //loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -161,4 +165,4 @@ const showRecipe = async () => {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
